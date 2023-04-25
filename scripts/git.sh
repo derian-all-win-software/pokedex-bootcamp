c5f68863d2b28e23700a66e0510674e65f84eb64
derian@all-win.software <derian@all-win-software>
2023-04-24 20:02:52 -0600

    feat: Added default value

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,5 +1,21 @@
-git add .
-git commit -m "mensaje"
-# git push 
+#!/bin/bash
 
-echo "Se ejecuto el script"
+default_has_push="false"
+
+files=$1
+commit_message=$2
+has_push=${3:-$default_has_push}
+
+echo "Adding files"
+git add "$files"
+
+echo "Making commit"
+git commit -m "$message" # TODO: Fix commit message
+
+echo "Has push: $has_push"
+
+if [[ "$has_push" == "true" ]]; then
+    current_branch=$(git branch --show-current)
+    echo "Pushing from $current_branch"
+    git push origin $current_branch
+fi
